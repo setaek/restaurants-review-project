8c8ecfb025fbee55ae897448e04fabb4e9a6829d
Kader <settahkader@gmail.com>
2020-01-06 20:36:15 +0100

    add service worker

--- js/sw_register.js
+++ js/sw_register.js
@@ -1,6 +1,8 @@
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("./sw.js")
-    .then(() => console.log("registration succeeded"))
-    .catch(() => console.log("registration failed"));
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("sw.js")
+      .then(() => console.log("registration succeeded"))
+      .catch(() => console.log("registration failed"));
+  });
 }
--- sw.js
+++ sw.js
@@ -1,30 +1,33 @@
-let restaurantCacheName = "restaurant-static-v2";
+let restaurantCacheName = "restaurant-static-v1";
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(restaurantCacheName).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./restaurant.html",
-        "./css/styles.css",
-        "./css/responsiveStyles.css",
-        "./data/restaurants.json",
-        "./js/dbhelper.js",
-        "./js/main.js",
-        "./js/restaurant_info.js",
-        "./js/sw_register.js",
-        "./img/1.jpg",
-        "./img/2.jpg",
-        "./img/3.jpg",
-        "./img/4.jpg",
-        "./img/5.jpg",
-        "./img/6.jpg",
-        "./img/7.jpg",
-        "./img/8.jpg",
-        "./img/9.jpg",
-        "./img/10.jpg"
-      ]);
+      return cache
+        .addAll([
+          "./",
+          "./index.html",
+          "./restaurant.html",
+          "./css/styles.css",
+          "./css/responsiveStyles.css",
+          "./data/restaurants.json",
+          "./js/dbhelper.js",
+          "./js/main.js",
+          "./js/restaurant_info.js",
+          "./js/sw_register.js",
+          "./img/1.jpg",
+          "./img/2.jpg",
+          "./img/3.jpg",
+          "./img/4.jpg",
+          "./img/5.jpg",
+          "./img/6.jpg",
+          "./img/7.jpg",
+          "./img/8.jpg",
+          "./img/9.jpg",
+          "./img/10.jpg"
+        ])
+        .then(() => console.log("Assets added to cache"))
+        .catch(err => console.log("Error while fetching assets", err));
     })
   );
 });
