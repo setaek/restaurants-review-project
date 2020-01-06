454164d8755bfa27fd114d6c39a7a4b6b3d931cd
Kader <settahkader@gmail.com>
2020-01-06 21:15:34 +0100

    add service worker

--- js/sw_register.js
+++ js/sw_register.js
@@ -1,8 +1,20 @@
+// if ("serviceWorker" in navigator) {
+//   window.addEventListener("load", function() {
+//     navigator.serviceWorker
+//       .register("./sw.js")
+//       .then(() => console.log("registration succeeded"))
+//       .catch(() => console.log("registration failed"));
+//   });
+// }
 if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
-    navigator.serviceWorker
-      .register("sw.js")
-      .then(() => console.log("registration succeeded"))
-      .catch(() => console.log("registration failed"));
-  });
+  navigator.serviceWorker
+    .register("./sw.js")
+    .then(reg => {
+      // registration worked
+      console.log("Registration succeeded. Scope is " + reg.scope);
+    })
+    .catch(error => {
+      // registration failed
+      console.log("Registration failed with " + error);
+    });
 }
